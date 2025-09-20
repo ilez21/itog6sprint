643bdf49d95c78d68861c94f9c4765714a85ae9b
admin <i.albagachiev@mail.ru>
2025-09-20 17:27:22 +0300

    6sprint

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	logger     *log.Logger
+	httpServer *http.Server
+}
+
+func New(logger *log.Logger) *Server {
+	router := http.NewServeMux()
+	router.HandleFunc("/", handlers.IndexHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		logger:     logger,
+		httpServer: srv,
+	}
+}
+
+func (s *Server) ListenAndServe() error {
+	return s.httpServer.ListenAndServe()
+}
